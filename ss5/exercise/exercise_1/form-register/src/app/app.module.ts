1547d3a8b5b5cbc1d3973a5cde3e3178c97fb0d3
Your Name <you@example.com>
2022-12-28 13:05:34 +0700

    exer 1

---
+++ ss5/exercise/exercise_1/form-register/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './component/common/register/register.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { LoginComponent } from './component/common/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ss5/exercise/exercise_1/form-register/src/model/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id?: number,
+  name?: string,
+  email?: string,
+  password?: string,
+  country?: string,
+  age?: string,
+  gender?: string,
+  phone?: string
+}
